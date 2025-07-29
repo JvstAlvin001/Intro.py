0a869a2bb57a03ab33502c3351df3eeb6464f4d2
JvstAlvin001 <macharialvin425@gmail.com>
2025-07-29 20:22:02 +0300

    first commit

---
+++ Intro.py
@@ -0,0 +1,40 @@
+#Define variables for a student in a programming course
+# PLP = "Academy"
+# Student_Name = "Joy Mungai"
+# Course_Name = "Python Programming"
+# Student_Id = 145731
+# Student_Age = 20 
+# Fees_Paid = "Paid"
+# # To comment out a line, use the hash symbol (#)
+# """To comment out multiple lines, use triple quotes.
+# This is a multi-line comment."""
+# # Ctrl + / can be used to comment or uncomment lines in many code editors.
+# print(Student_Name,Student_Id, Student_Age,Fees_Paid )
+# Languages = ["Python", "Java", "C++"]
+# print(Languages)
+# Shopping_List = ["Apples", "Bananas", "Oranges"]
+# print(Shopping_List)
+# #Tuples are immutable, meaning they cannot be changed after creation
+# # Student_Info = ("Joy Mungai", 145731, 20, "Python Programming")
+# # print(Student_Info)
+# #Sets are unordered collections of unique elements
+# Set_A = {1, 2, 3, 4, 5}
+# Set_B = {4, 5, 6, 7, 8}
+# #Union of two sets
+# print(union := Set_A | Set_B)  # Union of two sets
+#  # Intersection of two sets
+# print(intersection := Set_A & Set_B)
+# # Difference of two sets
+# print(difference := Set_A - Set_B)  # Elements in Set_A but not in Set_B
+# #Dictionaries store data in key-value pairs
+# Student_Details = {"Name": "Joy Mungai", "ID": 145731, "Age": 20, "Course": "Python Programming"}
+# print(Student_Details)
+# Capital_Cities = {"Kenya": "Nairobi", "Uganda": "Kampala", "Tanzania": "Dodoma"}
+# print(Capital_Cities)
+#Logical operators are used to combine conditional statements
+a = 10
+b = 20
+c = 30
+print((a < b) and (b < c))  # True if both conditions are true
+print((a < b) or (b > c))   # True if at least one condition is true
+print(not (a < b))           # True if the condition is false
